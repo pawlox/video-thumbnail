87c33221d518f97a9ee2468764bee9208fed2326
p.marciniak <p.marciniak@toucan-systems.pl>
2019-11-11 13:54:53 +0100

    Test Travis CI integration

---
+++ tests/Feature/VideoThumbnailTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vendor\Packages\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class VideoThumbnailTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
